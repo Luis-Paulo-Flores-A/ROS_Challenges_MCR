47025652095cc46e8a03817f138e39860c7f8b94
Luis-Paulo-Flores-A <80789093+Luis-Paulo-Flores-A@users.noreply.github.com>
2023-03-03 17:36:28 -0600

    Delete catkin_ws/build directory

--- catkin_ws/build/basic_comms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/ROS_Challenges_MCR/catkin_ws/src/basic_comms
-# Build directory: /home/luis/ROS_Challenges_MCR/catkin_ws/build/basic_comms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
